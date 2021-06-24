c91c99fb67642a5bbfe2ed3035fcac640fed4598
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-06-24 16:13:33 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,14 @@
+﻿using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    { }
+
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
